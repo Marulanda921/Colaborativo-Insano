6ec941598415d806776d5ed23daa86d715179fa2
Marulanda921 <alejandromarulanda702@gmail.com>
2024-02-25 23:07:12 -0500

    ventas

---
+++ storeZone/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navegacion from './Navegacion'
+
+function App() {
+
+
+  return (
+    <>
+    <Navegacion></Navegacion>
+    </>
+  )
+}
+
+export default App
---
+++ storeZone/src/Navegacion.jsx
@@ -0,0 +1,313 @@
+import React from 'react'
+
+function Navegacion() {
+  return (
+    <>
+  <nav class="bg-gray-800" >
+ 
+  </nav>
+
+
+   
+
+    <div className='bg-slate-50 h-1/2 p-14 pt-32 pb-0 flex justify-center flex-wrap md:flex-nowrap '>
+      <div class="relative flex w-3/4 h-full mb-9 mr-7 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div class="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+      src="https://images.ecestaticos.com/eBPOzAJ8yJP9pZDHxkB_GCKKnXs=/0x109:2118x1300/1200x675/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fae7%2F057%2Ff66%2Fae7057f662514c104cf253f096b86680.jpg"
+      class="h-full w-full object-cover"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      Filete de carne
+      </p>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+        $65.00
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+    El filete de carne es un corte de carne, generalmente de res, que se cocina a la parrilla, a la plancha o al horno. Es conocido por su sabor jugoso y tierno, y suele servirse con una variedad de acompañamientos, como verduras asadas, puré de papas o ensalada.
+    </p>
+  </div>
+  <div class="p-6 pt-0">
+    <button
+      class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>
+<div class="relative flex w-3/4 h-full flex-col mb-9 mr-7 rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div class="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+      src="https://d1uz88p17r663j.cloudfront.net/original/62133afe07415d90f01c9855f5fcd65e_PESCADO-A-LA-PARRILLA-SALSA-ASIATICA-2.png"
+      class="h-full w-full object-cover"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      Salmón a la parrilla
+      </p>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+        $95.00
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+    El salmón a la parrilla es un plato de pescado que se prepara asando filetes de salmón en una parrilla. El salmón a la parrilla es conocido por su sabor ahumado y su textura tierna. Se puede servir con una variedad de acompañamientos, como arroz, verduras asadas o ensalada.
+    </p>
+  </div>
+  <div class="p-6 pt-0">
+    <button
+      class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>
+<div class="relative flex w-3/4 h-full flex-col mb-9  mr-7 rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div class="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+      src="https://i.blogs.es/9ea7a4/pollo_curry-copia/650_1200.jpg"
+      class="h-full w-full object-cover"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      Pollo al curry
+      </p>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+        $95.00
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+    El pollo al curry es un plato de origen asiático que consiste en trozos de pollo cocinados en una salsa de curry, que generalmente incluye una mezcla de especias, leche de coco y otros ingredientes aromáticos. Se sirve comúnmente con arroz o pan naan..
+    </p>
+  </div>
+  <div class="p-6 pt-0">
+    <button
+      class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>
+<div class="relative flex w-3/4 h-1/2 flex-col mb-9 mr-7 rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div class="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+      src="https://badun.nestle.es/imgserver/v1/80/1290x742/fd236eb468b4-tagliatelle-con-salsa-alfredo.jpg"
+      class="h-full w-full object-cover"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      Tagliatelle con Salsa Alfredo
+      </p>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+        $95.00
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+    Los tagliatelle con salsa Alfredo son una pasta italiana servida con una salsa cremosa a base de mantequilla, nata, y queso parmesano. Es un plato rico y suculento.
+    </p>
+  </div>
+  <div class="p-6 pt-0">
+    <button
+      class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>
+<div class="relative flex w-3/4 h-1/2 flex-col mb-9 mr-7 rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div class="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+      src="https://www.gourmet.cl/wp-content/uploads/2016/12/Carbonara-editada.jpg"
+      class="h-full w-full object-cover"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      Pasta carbonara
+      </p>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+        $95.00
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+    La pasta carbonara es un plato de pasta italiano que se prepara con huevos, queso, panceta o tocino, y pimienta negra. La salsa cremosa se mezcla con la pasta caliente para crear un plato reconfortante y sabroso.
+    </p>
+  </div>
+  <div class="p-6 pt-0">
+    <button
+      class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>
+
+    </div>
+    <div className='bg-slate-50 h-1/2 p-14 flex justify-center flex-wrap md:flex-nowrap '>
+      <div class="relative flex w-3/4 h-1/2 mb-9 mr-7 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div class="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+      src="https://images.aws.nestle.recipes/original/c9ab563fd1fdc48bf63c2363921301ac_pechuga_de_pollo_rellena.jpeg"
+      class="h-full w-full object-cover"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      Pechuga de Pollo Rellena
+      </p>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+        $95.00
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+    La pechuga de pollo rellena es un plato que consiste en pechugas de pollo deshuesadas y rellenas con una mezcla de ingredientes como espinacas, queso, jamón, y hierbas. Se cocina al horno y se sirve con una variedad de salsa.
+    </p>
+  </div>
+  <div class="p-6 pt-0">
+    <button
+      class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>
+<div class="relative flex w-3/4 h-1/2 flex-col mb-9 mr-7 rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div class="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+      src="https://polloseldorado.co/wp-content/uploads/2023/10/1-1-1-1024x536.jpg"
+      class="h-full w-full object-cover"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      Risotto de champiñones
+      </p>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+        $95.00
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+    El risotto de champiñones es un plato italiano a base de arroz arborio cocinado lentamente en caldo hasta que alcanza una consistencia cremosa. Se mezcla con champiñones salteados, cebolla, ajo, vino blanco y queso parmesano, lo que le confiere un sabor rico y reconfortante..
+    </p>
+  </div>
+  <div class="p-6 pt-0">
+    <button
+      class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>
+<div class="relative flex w-3/4 h-1/2 flex-col mb-9  mr-7 rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div class="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+      src="https://images.hola.com/imagenes/cocina/recetas/20210318186295/lomo-de-cerdo-asado-al-horno/0-932-27/diapdre-home-t.jpg"
+      class="h-full w-full object-cover"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      Lomo de Cerdo Asado
+      </p>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+        $95.00
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+    El lomo de cerdo asado es un corte de carne de cerdo que se cocina lentamente en el horno o a la parrilla. Se sazona con hierbas y especias, lo que le confiere un sabor distintivo y una textura tierna.
+    </p>
+  </div>
+  <div class="p-6 pt-0">
+    <button
+      class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>
+<div class="relative flex w-3/4 h-1/2 flex-col mb-9 mr-7 rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div class="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+      src="https://assets.unileversolutions.com/recipes-v2/153041.jpg"
+      class="h-full w-full object-cover"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      Pescado a la Veracruzana
+      </p>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+        $95.00
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+    El pescado a la Veracruzana es un plato mexicano que consiste en pescado (generalmente huachinango o pargo) cocinado con tomate, cebolla, aceitunas, alcaparras, chiles y especias. Es un plato sabroso y picante.
+    </p>
+  </div>
+  <div class="p-6 pt-0">
+    <button
+      class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>
+<div class="relative flex w-3/4 h-1/2 flex-col mb-9 mr-7 rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div class="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+      src="https://imag.bonviveur.com/espaguetis-a-la-bolonesa.jpg"
+      class="h-full w-full object-cover"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+      Espaguetis Boloñesa
+      </p>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+        $95.00
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+    Los espaguetis a la boloñesa son una pasta italiana servida con una salsa de carne cocida a fuego lento con tomate, cebolla, zanahoria, apio, vino tinto y hierbas aromáticas. Es un plato clásico reconfortante y abundante.
+    </p>
+  </div>
+  <div class="p-6 pt-0">
+    <button
+      class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>
+
+    </div>
+    </>
+  )
+}
+
+export default Navegacion
